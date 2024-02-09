ffeb86226fa0137f3ef8a6061311f5a38e1fb955
Dawid Blocher <dawid.blocher@gmail.com>
2024-02-09 15:21:04 +0100

    test

--- src/graphql/schema/resolvers/Query/product.ts
+++ src/graphql/schema/resolvers/Query/product.ts
@@ -1,14 +1,9 @@
 import type { QueryResolvers } from "./../../../types.generated";
-import { prisma } from "../../../../db.js";
+
 export const product: NonNullable<QueryResolvers["product"]> = async (
   _parent,
   _arg,
   _ctx,
 ) => {
-  const product = await prisma.product.findUnique({
-    where: {
-      id: _arg.id,
-    },
-  });
-  return product;
+  //
 };
